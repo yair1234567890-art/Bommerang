9fc5b1361f63f9fee5c2c8c56abfa1bbc9078b56
yair1234567890-art <77450433+yair1234567890-art@users.noreply.github.com>
2021-04-08 20:14:46 +0300

    Add files via upload

---
+++ Bommerang.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bommerang : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    public Vector3 offset;
+    Vector3 pos;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            pos = transform.position;
+            agent.SetDestination(transform.position + offset);
+            StartCoroutine(W());
+        }
+    }
+
+    IEnumerator W()
+    {
+        agent.SetDestination(transform.position + offset);
+        yield return new WaitForSeconds(1f);
+        agent.SetDestination(pos);
+    }
+}
